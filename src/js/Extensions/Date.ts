4cab87343ed299bb0be9aedfac457bf1858cb4f5
Evgeny Tkachenko <evgeny.tk@mail.ru>
2019-04-18 19:51:45 +0300

    Added: new methods to Date prototype

--- src/js/Extensions/Date.ts
+++ src/js/Extensions/Date.ts
@@ -1,7 +1,11 @@
 interface Date {
   resetTime(): Date,
+  resetMonth(): Date,
+  resetYear(): Date,
+  resetDecade(): Date,
   diffInDays(date: Date): number,
   addDays(count: number): Date,
+  addYears(count: number): Date,
   isToday(): boolean,
   dayInMonth(date: Date): boolean
 }
@@ -10,6 +14,27 @@ Date.prototype.resetTime = function(): Date {
   const timestamp: number = new Date(this).setHours(0, 0, 0, 0);
 
   return new Date(timestamp);
+}
+
+Date.prototype.resetMonth = function(): Date {
+  let timestamp: number = new Date(this).setDate(1);
+  return new Date(timestamp);
+}
+
+Date.prototype.resetYear = function(): Date {
+  let timestamp: number = new Date(this).setMonth(0);
+      timestamp = new Date(this).setDate(0);
+
+  return new Date(timestamp);
+}
+
+Date.prototype.resetDecade = function(): Date {
+  let date = new Date(this);
+  while(date.getFullYear() % 10 !== 0) {
+    date = new Date(date.setFullYear(date.getFullYear() - 1));
+  }
+
+  return date;
 }
 
 Date.prototype.diffInDays = function(date: Date): number {
@@ -26,6 +51,16 @@ Date.prototype.addDays = function(count: number): Date {
     return new Date(timestamp + (3600 * 24 * count * 1000));
   } else {
     return new Date(timestamp - (3600 * 24 * Math.abs(count) * 1000));
+  }
+}
+
+Date.prototype.addYears = function(count: number): Date {
+  const date: Date = new Date(this);
+
+  if(count >= 0) {
+    return new Date(date.setFullYear(date.getFullYear() + count));
+  } else {
+    return new Date(date.setFullYear(date.getFullYear() - Math.abs(count)));
   }
 }
 
