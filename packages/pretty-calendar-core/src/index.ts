7a35c0da23ef0282cb068dc36283b7136472b3ab
Evgeny Tkachenko <evgeny.tk@mail.ru>
2019-05-03 17:15:37 +0300

    Updated: folder structure in pretty-calendar-core package

--- packages/pretty-calendar-core/src/index.ts
+++ packages/pretty-calendar-core/src/index.ts
@@ -1,4 +1,4 @@
-export { IIntl } from './IIntl';
+export { IIntl } from './Intl/IIntl';
 export { IGridOptions } from './Grid/IGridOptions';
 export { WeekdaysEnum } from './Grid/WeekdaysEnum';
 export { default as Grid } from './Grid/Grid';
@@ -6,3 +6,5 @@ export { default as Grid } from './Grid/Grid';
 export { IMaskCharInterface } from './Mask/IMaskCharInterface';
 export { IMaskOptions } from './Mask/IMaskOptions';
 export { default as Mask } from './Mask/Mask';
+
+export * from './utils/date-helpers';
